4a7002f5c4f5c462bfec4b719a11e58f88519677
Brendan Griffiths <orwellian225@gmail.com>
2025-08-24 21:14:09 +0200

    change!: cleaner CLI interface with future commands marked as todo

--- src/main.rs
+++ src/main.rs
@@ -10,33 +10,35 @@ use crate::notebook::chapter::Chapter;
 
 #[derive(Parser)]
 #[command(version, about)]
-struct Cli {
+struct SkychainCli {
     #[command(subcommand)]
-    command: Commands
+    command: Option<Commands>
 }
 
 #[derive(Subcommand)]
 enum Commands {
     /// Initialize a new skychain notebook.
-    Init {
-        name: Option<String>,
-    },
-
-    /// Create new components in the notebook
-    New {
+    Init { title: Option<String> },
+    /// Add components to the notebook
+    Add {
         #[command(subcommand)]
-        subcommand: NewCommand
+        subcommand: AddCommand
+    },
+    /// Modify the config of the notebook
+    Config {
+        #[arg(short, long)]
+        global: bool
     }
 }
 
 #[derive(Subcommand)]
-enum NewCommand {
-    Page { name: String },
+enum AddCommand {
+    Page { title: String },
     Chapter { title: String }
 }
 
 fn main() {
-    let cli = Cli::parse();
+    let cli = SkychainCli::parse();
     let current_dir = match current_dir() {
         Ok(val) => val,
         Err(err) => {
@@ -46,19 +48,25 @@ fn main() {
     };
 
     match &cli.command {
-        Commands::Init { name } => { 
-            let _notebook = Notebook::init_notebook(&current_dir, name.to_owned());
-        }
-        Commands::New { subcommand } => { 
-            let mut notebook = Notebook::load_notebook(&current_dir);
-            match subcommand {
-                NewCommand::Page{ name } => { 
-                    let new_page = Page::create_page(current_dir, name.to_owned());
-                    notebook.pages_mut().push(new_page);
-                },
-                NewCommand::Chapter { title } => {
-                    let new_chapter = Chapter::create_chapter(current_dir, title.to_owned());
-                    notebook.chapters_mut().push(new_chapter);
+        None => { todo!(); }
+        Some(cmd) => match cmd {
+            Commands::Init { title: name } => { 
+                let _notebook = Notebook::init_notebook(&current_dir, name.to_owned());
+            },
+            Commands::Config { global } => {
+                todo!();
+            }
+            Commands::Add { subcommand } => { 
+                let mut notebook = Notebook::load_notebook(&current_dir);
+                match subcommand {
+                    AddCommand::Page{ title: name } => { 
+                        let new_page = Page::create_page(current_dir, name.to_owned());
+                        notebook.pages_mut().push(new_page);
+                    },
+                    AddCommand::Chapter { title } => {
+                        let new_chapter = Chapter::create_chapter(current_dir, title.to_owned());
+                        notebook.chapters_mut().push(new_chapter);
+                    }
                 }
             }
         }
